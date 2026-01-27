f2ccb789fb287473d035f9a54fdb1e6034a9889f
Srinidhi-codes <66681637+Srinidhi-codes@users.noreply.github.com>
2026-01-28 01:08:54 +0530

    feat: Migrate authentication from custom middleware to a new system and add API rewrites.

---
+++ auth.config.ts
@@ -0,0 +1,13 @@
+export default {
+  matcher: [
+    "/dashboard/:path*",
+    "/attendance/:path*",
+    "/employees/:path*",
+    "/leaves/:path*",
+    "/payroll/:path*",
+    "/profile/:path*",
+    "/analytics/:path*",
+    "/login",
+    "/register",
+  ],
+};
